cae10c127610abffe32b74d3cdce1947898d2aed
virtualconnekt <virtualconnekt@gmail.com>
2025-08-01 09:06:42 +0100

    Delete backend directory

--- backend/routes/widgetPaymentRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createWidgetPayment, verifyWidgetPayment } from '../controllers/widgetPaymentController.js';
-
-const router = express.Router();
-
-router.post('/create', createWidgetPayment);
-router.post('/verify/:id', verifyWidgetPayment);
-
-export default router; 
